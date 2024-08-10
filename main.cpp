576d3ddbe6aff5a8c29543d94d8e3ffd8e872ea1
kakaintest <168247648+kakaintest@users.noreply.github.com>
2024-08-10 22:45:35 +0700

    Submit code at 23:44:35 10/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,62 @@
-/*
-    HiCommit - Problem 1
-    Available at: https://hicommit.kakanie.com/problem/1
-*/
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+long long multi (long long a, long long b, long long m) {
+	if (b==0) return 0;
+	long long t = multi (a, b/2, m) % m;
+	if (b % 2 == 1) {
+		return ((t+t)%m+a%m)%m;
+	}
+	else {
+		return (t+t)%m;
+	}
+}
 
-    /*
-        Your code goes here. Happy coding!
-    */
+void matrix_multiplication(long long A[2][2], long long B[2][2], long long M) {
+  long long C[2][2];
+  for (long long i = 0; i < 2; i++) {
+    for (long long j = 0; j < 2; j++) {
+      C[i][j] = 0;
+      for (long long k = 0; k < 2; k++) {
+        C[i][j] += multi (A[i][k], B[k][j], M);
+        C[i][j]%=M;
+      }
+    }
+  }
+  for (long long i = 0; i < 2; i++) {
+    for (long long j = 0; j < 2; j++) {
+      A[i][j] = C[i][j];
+    }
+  }
+}
 
-    return 0;
+long long fib(long long n, long long m) {
+  if (n <= 1) {
+    return n;
+  }
+  long long F[2][2] = {{1, 1}, {1, 0}};
+  long long result[2][2] = {{1, 0}, {0, 1}};
+  while (n > 0) {
+    if (n % 2 == 1) {
+      matrix_multiplication(result, F, m);
+    }
+    matrix_multiplication(F, F, m);
+    n /= 2;
+  }
+  return result[0][1];
+}
+
+void solve () {
+	long long a, b, m, k;
+	cin >> a >> b >> m;
+	k = __gcd(a, b);
+	cout << fib(k, m) << endl;
+}
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);  cout.tie(0);
+	solve();
+	return 0;
 }
